01d434651aaf1abb03468bc5d21a8d7478bbcedc
Junevy <junev.chung@gmail.com>
2026-07-18 23:44:17 +0800

    Fix: Fix some bugs of TabControl Adapter

--- Common/Adapters/TabControlRegionAdapter.cs
+++ Common/Adapters/TabControlRegionAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using SimpleNavigation.Interface.Adapters;
+using System.Collections;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,7 +14,7 @@ namespace SimpleNavigation.Common.Adapters
 
         public void Present(FrameworkElement host, FrameworkElement content)
         {
-            if (host is not TabControl tabControl)
+            if (host is not TabControl tabControl || content is not UserControl view)
             {
                 throw new ArgumentException(
                     $"Host type '{host.GetType().FullName}' must be a TabControl.",
@@ -25,18 +27,32 @@ namespace SimpleNavigation.Common.Adapters
             {
                 tabControl.Items.Add(content);
             }
-            //else
-            //{
-            //    if (tabControl.ItemsSource is IList list)
-            //    {
-            //        list.Add(list);
-            //    }
+            else
+            {
+                if (tabControl.ItemsSource is IList list)
+                {
+                    //list.Add(new TabItem() { Header = "new tab", Content = view});
+                    if (list.Count <= 0)
+                        throw new ArgumentException("The tab control must be keep at least 1 item");
 
-            //    if (tabControl.ItemsSource is IEnumerable enums)
-            //    {
-            //        enums.
-            //    }
-            //}
+                    var targetType = list[0]?.GetType();
+                    object instance = Activator.CreateInstance(targetType);
+                    if (instance != null)
+                    {
+                        FieldInfo contentFi = targetType.GetField("ContentProperty", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                        DependencyProperty contentDp = (DependencyProperty)contentFi.GetValue(null);
+
+                        FieldInfo headerFi = targetType.GetField("HeaderProperty", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                        DependencyProperty headerDp = (DependencyProperty)headerFi.GetValue(null);
+
+                        var header = content.GetType().Name.Replace("View", "");
+
+                        ((DependencyObject)instance).SetValue(headerDp, header);
+                        ((DependencyObject)instance).SetValue(contentDp, content);
+                        list.Add(instance);
+                    }
+                }
+            }
         }
     }
 }
